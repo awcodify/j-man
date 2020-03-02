e5af6b432e3cd623ac1da8d58ff770de39b3bfe6
awcodify <awcodify@gmail.com>
2020-03-02 10:37:31 +0700

    define basepath for handle config inside `app/` directories

--- config/enviroments.go
+++ config/enviroments.go
@@ -4,10 +4,17 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
+	"runtime"
 	"strings"
 
 	"github.com/awcodify/j-man/utils"
 	"gopkg.in/yaml.v2"
+)
+
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basepath   = filepath.Join(filepath.Dir(b), "../")
 )
 
 // Config will be seperated between production and development
@@ -21,8 +28,8 @@ type Config struct {
 			Port string `yaml:"port"`
 		}
 	}
-	HTML HTML     `yaml:"HTML"`
-	DB   Database `yaml:"DB"`
+	HTML HTML       `yaml:"HTML"`
+	DB   Postgresql `yaml:"DB"`
 }
 
 // New will instantiate config from production or development
@@ -30,7 +37,7 @@ func New() (c Config) {
 	env := os.Getenv("APP_ENV")
 
 	fileName := fmt.Sprintf("config.%s.yaml", strings.ToLower(env))
-	file, err := os.Open(fileName)
+	file, err := os.Open(basepath + "/" + fileName)
 	utils.DieIf(err)
 
 	if env == "" {
