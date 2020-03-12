e3d19fd880f996a1597136d52d9c43ae2bff1ddc
awcodify <awcodify@gmail.com>
2020-03-12 12:25:48 +0700

    cover view test

---
+++ app/views/run_test.go
@@ -0,0 +1,24 @@
+package views
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/awcodify/j-man/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunHandler(t *testing.T) {
+	cfg := config.New()
+	cfg.HTML.Root = "html"
+	cfg.HTML.Layout.BaseHTML = "html/layout/base.html"
+	v := View{Config: cfg}
+
+	rr := httptest.NewRecorder()
+	req := &http.Request{Method: "GET"}
+
+	v.RunHandler(rr, req)
+
+	assert.HTTPBodyContains(t, v.RunHandler, "GET", "/run", nil, "Run Test!")
+}
