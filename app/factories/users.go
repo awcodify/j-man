136402093f4a298a2715afc2442a41046d31f4cc
awcodify <awcodify@gmail.com>
2020-03-12 12:27:22 +0700

    create user factory for test

---
+++ app/factories/users.go
@@ -0,0 +1,43 @@
+package factories
+
+import (
+	"context"
+
+	"github.com/awcodify/j-man/app/models"
+	"github.com/awcodify/j-man/config"
+	"github.com/awcodify/j-man/utils"
+	"github.com/khaiql/dbcleaner"
+	"github.com/khaiql/dbcleaner/engine"
+	"github.com/volatiletech/sqlboiler/boil"
+)
+
+var (
+	cfg     = config.New()
+	db, _   = cfg.ConnectDB()
+	Cleaner = dbcleaner.New()
+)
+
+type User struct {
+	models.User
+}
+
+func (u User) Create(fn func()) {
+	psql := engine.NewPostgresEngine(cfg.DB.DSN)
+	Cleaner.SetEngine(psql)
+	Cleaner.Acquire("users")
+
+	if u.Email == "" {
+		u.Email = "example@email.com"
+	}
+
+	if u.PasswordDigest == "" {
+		u.PasswordDigest = "passwordHash"
+	}
+
+	err := u.Insert(context.Background(), db, boil.Infer())
+	utils.DieIf(err)
+
+	fn()
+
+	Cleaner.Clean("users")
+}
