cbf2644ae8ffc4e65e9f6eab9eabd2d59ad464ef
awcodify <awcodify@gmail.com>
2020-03-13 15:40:34 +0700

    simplify throughput calculator

--- calculator/throughput.go
+++ calculator/throughput.go
@@ -5,27 +5,23 @@ import (
 )
 
 const (
-	toSecond  = 1000
-	toMinutes = toSecond * 60
+	toSecond = 1000
+	toMinute = toSecond * 60
 )
 
 func (r *Result) RPS() float64 {
-	return r.rate(func(milisecond float64) float64 {
-		return milisecond * toSecond
-	})
+	return r.rate() * toSecond
 }
 
 func (r *Result) RPM() float64 {
-	return r.rate(func(milisecond float64) float64 {
-		return milisecond * toMinutes
-	})
+	return r.rate() * toMinute
 }
 
 func round(num float64) int {
 	return int(num + math.Copysign(0.5, num))
 }
 
-func (r Result) rate(fn func(float64) float64) float64 {
+func (r Result) rate() float64 {
 	numberOfRequests := float64(len(r.ResponseTimes))
-	return fn(numberOfRequests / (r.Average() * numberOfRequests))
+	return numberOfRequests / (r.Average() * numberOfRequests)
 }
