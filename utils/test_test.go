0d0c8fb82d81cbd2a357bc2ec0f93210898c1e92
awcodify <awcodify@gmail.com>
2020-03-20 16:57:41 +0700

    add dbCleaner test

---
+++ utils/test_test.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTestDBCleaner(t *testing.T) {
+	dsn := "user=jmanager_user password=password dbname=jmanager sslmode=disable"
+	db, _ := sql.Open("postgres", dsn)
+
+	TestDBCleaner(dsn, []string{"users"}, func() {
+		db.Exec(`INSERT INTO users (email) VALUES ("test@example.com")`)
+	})
+
+	var count int
+	row := db.QueryRow("SELECT COUNT(*) FROM users")
+	row.Scan(&count)
+
+	assert.Equal(t, 0, count)
+}
